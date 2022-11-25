891a032932f799e08e38e05ad0401203b29179a7
syahidnurrohim <syahidnurrohim@gmail.com>
2022-11-25 15:25:59 +0700

    update

--- SIDIAv2/MainWindow.xaml.cs
+++ SIDIAv2/MainWindow.xaml.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -14,45 +13,68 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using Wpf.Ui.Common;
-using Wpf.Ui.Controls;
 using Wpf.Ui.Controls.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
 
 
-namespace SIDIAv2
-{
+namespace SIDIAv2;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    /// 
+
+    public partial class MainWindow : INavigationWindow
     {
-        public MainWindow()
+        public MainWindow(INavigationService navigationService)
         {
             InitializeComponent();
-            MainFrame.Navigate(new Dashboard());
-        }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+            navigationService.SetNavigationControl(RootNavigation);
+
+        }
+        public Frame GetFrame()
+            => RootFrame;
+
+        public INavigation GetNavigation()
+            => RootNavigation;
+
+        public bool Navigate(Type pageType)
+            => RootNavigation.Navigate(pageType);
+
+        public void SetPageService(IPageService pageService)
+            => RootNavigation.PageService = pageService;
+
+        public void ShowWindow()
+            => Show();
+
+        public void CloseWindow()
+            => Close();
+
+        private void TrayMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
+            if (sender is not MenuItem menuItem)
+                return;
+
+            System.Diagnostics.Debug.WriteLine($"DEBUG | WPF UI Tray clicked: {menuItem.Tag}", "Wpf.Ui.Demo");
         }
 
         private void SidebarNavigate_Click(object sender, RoutedEventArgs e)
         {
-            NavigationItem navigationItem = (NavigationItem)sender;
-            string name = navigationItem.Name;
-            Page page = new Dashboard();
+        }
 
-            switch (name)
-            {
-                case "Home":
-                    page = new Dashboard();
-                    break;
-                case "Product":
-                    page = new Product();
-                    break;
-            }
 
-            MainFrame.Navigate(page);
+        private void RootNavigation_OnNavigated(INavigation sender, RoutedNavigationEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"DEBUG | WPF UI Navigated to: {sender?.Current ?? null}", "Wpf.Ui.Demo");
+
+            // This funky solution allows us to impose a negative
+            // margin for Frame only for the Dashboard page, thanks
+            // to which the banner will cover the entire page nicely.
+            RootFrame.Margin = new Thickness(
+                left: 0,
+                top: sender?.Current?.PageTag == "dashboard" ? -69 : 0,
+                right: 0,
+                bottom: 0);
         }
 
     }
-}
